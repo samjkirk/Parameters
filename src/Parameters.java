b02fd4cae0e7d834b2c156ddd79cb06a58c2bf80
Sam Kirk <samueljkirk10@gmail.com>
2018-05-17 10:47:21 +0100

    Version 1 - Parameters excercise

---
+++ src/Parameters.java
@@ -0,0 +1,9 @@
+public class Parameters {
+    public static void main(String[] args) {
+        printMessage("Hello World!");
+    }
+
+    static void printMessage(String message) {
+        System.out.println(message);
+    }
+}
